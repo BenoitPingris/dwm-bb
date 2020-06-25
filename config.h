2dc788da2b5090b7d4365aa0579738693116e3cf
Benoit Pingris <benoit.pingris@epitech.eu>
2020-06-25 16:16:35 +0200

    idk

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Hack:size=11";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#263238";
+static const char col_gray2[]       = "#546e7a";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#282a36";
